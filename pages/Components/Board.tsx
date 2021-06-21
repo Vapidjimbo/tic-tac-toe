cf7fa733db987ad969150a3747559d1848bb8e4e
Vapidjimbo <56098158+Vapidjimbo@users.noreply.github.com>
2021-06-20 23:25:07 -0700

    Adding All

---
+++ pages/Components/Board.tsx
@@ -0,0 +1,216 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
+import Square from "./Square";
+
+//Board Component
+
+//Component
+
+export default function Board() {
+  //state
+  const [isXTurn, setIsXTurn] = useState<boolean>(true),
+    [squareData, setSquareData] = useState<any>({}),
+    [turnText, setTurnText] = useState<string>("X's Turn"),
+    [gameOver, setGameOver] = useState<boolean>(false),
+    [reset, setReset] = useState<boolean>(false);
+
+  //methods
+  const checkTurn = (): void => {
+    isXTurn === false ? setIsXTurn(true) : setIsXTurn(false);
+  };
+
+  let gameReset = () => {
+    setIsXTurn(true);
+    setSquareData({});
+    setTurnText("X's Turn");
+    setGameOver(false);
+    setReset(true);
+  };
+
+  useEffect(() => {
+    isXTurn ? setTurnText("X's Turn") : setTurnText("O's Turn");
+  }, [isXTurn]);
+
+  useEffect(() => {
+    console.log(squareData);
+    //0,1,2; 0,3,6; 0,4,8
+    //1,4,7;
+    //2,5,8; 2,4,6
+    //3,4,5; 6,7,8
+    squareData[0] === "X" &&
+      squareData[1] === "X" &&
+      squareData[2] === "X" &&
+      setTurnText("X Wins!");
+    squareData[0] === "O" &&
+      squareData[1] === "O" &&
+      squareData[2] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[0] === "X" &&
+      squareData[3] === "X" &&
+      squareData[6] === "X" &&
+      setTurnText("X Wins!");
+    squareData[0] === "O" &&
+      squareData[3] === "O" &&
+      squareData[6] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[0] === "X" &&
+      squareData[4] === "X" &&
+      squareData[8] === "X" &&
+      setTurnText("X Wins!");
+    squareData[0] === "O" &&
+      squareData[4] === "O" &&
+      squareData[8] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[1] === "X" &&
+      squareData[4] === "X" &&
+      squareData[7] === "X" &&
+      setTurnText("X Wins!");
+    squareData[1] === "O" &&
+      squareData[4] === "O" &&
+      squareData[7] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[2] === "X" &&
+      squareData[5] === "X" &&
+      squareData[8] === "X" &&
+      setTurnText("X Wins!");
+    squareData[2] === "O" &&
+      squareData[5] === "O" &&
+      squareData[8] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[2] === "X" &&
+      squareData[4] === "X" &&
+      squareData[6] === "X" &&
+      setTurnText("X Wins!");
+    squareData[2] === "O" &&
+      squareData[4] === "O" &&
+      squareData[6] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[3] === "X" &&
+      squareData[4] === "X" &&
+      squareData[5] === "X" &&
+      setTurnText("X Wins!");
+    squareData[3] === "O" &&
+      squareData[4] === "O" &&
+      squareData[5] === "O" &&
+      setTurnText("O Wins!");
+
+    squareData[6] === "X" &&
+      squareData[7] === "X" &&
+      squareData[8] === "X" &&
+      setTurnText("X Wins!");
+    squareData[6] === "O" &&
+      squareData[7] === "O" &&
+      squareData[8] === "O" &&
+      setTurnText("O Wins!");
+  }, [squareData]);
+
+  useEffect(() => {
+    //objectLength = Object.keys(exampleObject).length
+    console.log(Object.keys(squareData).length);
+    if(Object.keys(squareData).length === 9) {
+
+      setTurnText("Draw!");
+    }
+  }, [squareData]);
+
+  useEffect(() => {
+    turnText === "X Wins!" && setGameOver(true);
+    turnText === "O Wins!" && setGameOver(true);
+    turnText === "Draw!" && setGameOver(true);
+  }, [turnText]);
+
+  //jsx
+  return (
+    <>
+      <p>
+        {turnText}
+        <style jsx>{`
+          p {
+            font-size: x-large;
+            text-decoration: underline;
+            font-weight: bold;
+            color: #19dae6;
+        `}</style>
+      </p>
+
+      <div>
+        {[...Array(9)].map((value: undefined, index: number) => (
+          <Square
+            key={index}
+            squareId={index.toString()}
+            isXTurn={isXTurn}
+            checkTurn={checkTurn}
+            setSquareData={setSquareData}
+            squareData={squareData}
+            gameOver={gameOver}
+            reset={reset}
+            setReset={setReset}
+          />
+        ))}
+        <style jsx>{`
+          div {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            border-radius: 4px;
+            max-width: 40%;
+            max-height: auto;
+            background: rgb(36, 36, 36);
+          }
+
+          @media only screen and (max-width: 880px) {
+            div {
+              min-width: 50%;
+            }
+          }
+
+          @media only screen and (max-width: 688px) {
+            div {
+              min-width: 60%;
+            }
+          }
+
+          @media only screen and (max-width: 576px) {
+            div {
+              min-width: 75%;
+            }
+          }
+
+          @media only screen and (max-width: 460px) {
+            div {
+              min-width: 100%;
+            }
+          }
+        `}</style>
+      </div>
+
+      <button onClick={gameReset}>
+        Reset Game
+        <style jsx>{`
+          button {
+            margin-top: 1em;
+            background: rgb(36, 36, 36);
+            color: #19dae6;
+            border: 4px solid #19dae6;
+            border-radius: 4px;
+            font-size: large;
+            font-weight: bold;
+            cursor: pointer;
+          }
+          button:hover {
+            color: black;
+            background: #19dae6;
+          }
+        `}</style>
+      </button>
+    </>
+  );
+}
---
+++ pages/Components/Square.tsx
@@ -0,0 +1,141 @@
+import PropTypes from "prop-types";
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState, useReducer } from "react";
+import styles from "../styles/Home.module.css";
+
+//Square Child Component
+
+//prop types - typescript
+type SquareProps = {
+  checkTurn: any;
+  isXTurn: boolean;
+  squareId: string;
+  setSquareData: any;
+  squareData: object;
+  gameOver: boolean;
+  reset: boolean;
+  setReset: any;
+};
+
+//Component
+export default function Square({
+  checkTurn,
+  isXTurn,
+  squareId,
+  setSquareData,
+  squareData,
+  gameOver,
+  reset,
+  setReset,
+}: SquareProps) {
+  const [content, setContent] = useState<string>(""),
+    [played, setPlayed] = useState<boolean>(false);
+
+  useEffect(() => {
+    gameOver && setPlayed(true);
+  }, [gameOver]);
+
+  useEffect(() => {
+    if (reset === true) {
+      setContent("");
+      setPlayed(false);
+      setReset(false);
+    }
+  }, [reset]);
+
+  let modifyContent = (event: any) => {
+    let thisSquare = squareId;
+    let squareClicked = event.target.id;
+
+    if (squareClicked === thisSquare) {
+      isXTurn ? setContent("X") : setContent("O");
+      isXTurn
+        ? setSquareData({ ...squareData, [squareId]: "X" })
+        : setSquareData({ ...squareData, [squareId]: "O" });
+      setPlayed(true);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        id={squareId}
+        disabled={played}
+        onClick={(event: any) => {
+          checkTurn();
+          modifyContent(event);
+        }}
+      >
+        {content}
+      </button>
+      <style jsx>{`
+        div {
+          border: 19px outset #50ecff;
+          border-radius: 100px;
+          padding: 0.1em;
+          margin: 0.5em;
+          height: 12vh;
+          width: 12vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          box-shadow: 10px -1px 74px 10px rgba(19, 232, 254, 0.21);
+          -webkit-box-shadow: 10px -1px 74px 10px rgba(19, 232, 254, 0.21);
+          -moz-box-shadow: 10px -1px 74px 10px rgba(19, 232, 254, 0.21);
+        }
+        button {
+          font-size: xx-large;
+          border: none;
+          background: white;
+          border-radius: 100px;
+          height: 100%;
+          width: 100%;
+          cursor: pointer;
+          box-shadow: 18px 18px 27px -25px rgba(0, 0, 0, 0.65) inset;
+          -webkit-box-shadow: 18px 18px 27px -25px rgba(0, 0, 0, 0.65) inset;
+          -moz-box-shadow: 18px 18px 27px -25px rgba(0, 0, 0, 0.65) inset;
+        }
+        button:disabled {
+          color: black;
+          cursor: no-drop;
+        }
+
+        @media only screen and (max-width: 434px) {
+          div {
+            height: 11.5vh;
+            width: 11.5vh;
+          }
+        }
+
+        @media only screen and (max-width: 350px) {
+          div {
+            height: 10.5vh;
+            width: 10.5vh;
+          }
+        }
+
+        @media only screen and (max-width: 310px) {
+          div {
+            height: 9vh;
+            width: 9vh;
+          }
+        }
+
+        @media only screen and (max-width: 284px) {
+          div {
+            height: 8vh;
+            width: 8vh;
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
+
+//PropTypes - React
+Square.propTypes = {
+  checkTurn: PropTypes.func.isRequired,
+  isXTurn: PropTypes.bool.isRequired,
+  squareId: PropTypes.string.isRequired,
+};
